47d7143c28242988c9b28cea983d533b6f58cadb
Jung Hoon Shin <hidden110585@gmail.com>
2019-12-10 23:01:44 +0900

    gitinore 등록

---
+++ src/main/java/com/hidden/book/springboot/web/HelloController.java
@@ -0,0 +1,20 @@
+package com.hidden.book.springboot.web;
+
+import com.hidden.book.springboot.web.dto.HelloReponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloReponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloReponseDto(name, amount);
+    }
+}
---
+++ src/main/java/com/hidden/book/springboot/web/dto/HelloReponseDto.java
@@ -0,0 +1,13 @@
+package com.hidden.book.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloReponseDto {
+
+    private final String name;
+    private final int amount;
+
+}
